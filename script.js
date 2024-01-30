be9e1f0b82814bdd7c976cd15a9f20da3d1e2ecc
ryan <rhynogolfer42@gmail.com>
2024-01-29 20:42:23 -0800

    Completed

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const wrapper = document.querySelector('.wrapper'),
     qrInput = document.querySelector('.form input'),
     generateBtn = document.querySelector('.form button'),
-    qrImg = document.querySelector('.qr-code img')
+    qrImg = document.querySelector('.qr-code img');
 
 generateBtn.addEventListener('click', () => {
     let qrValue = qrInput.value;
     if (!qrValue) return;  // if the input is empty then return from here
-    // passing the api returned img src to qrImg
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
-    wrapper.classList.add('active');
+    generateBtn.innerText = "Generating QR Code..."; // Use innerText instead of innerHTML
+    // passing the API returned img src to qrImg
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+    qrImg.addEventListener('load', () => {
+        wrapper.classList.add('active');
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value) {
+        wrapper.classList.remove('active');
+    }
 });
